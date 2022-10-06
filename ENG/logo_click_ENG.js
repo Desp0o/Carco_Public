b213f405b18ea943325f6b237c7b64036f9f7092
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2022-10-06 14:27:50 +0400

    Add files via upload

---
+++ ENG/logo_click_ENG.js
@@ -0,0 +1,4 @@
+document.querySelector('.logoeng').addEventListener('click',()=>{
+    window.location.href = './ENG_MAIN_PAGE.html'
+    
+})
---
+++ RUS/logo_click_RUS.js
@@ -0,0 +1,4 @@
+document.querySelector('.navbar_logo_img').addEventListener('click',()=>{
+    window.location.href = './RUS_main.html'
+    
+})
